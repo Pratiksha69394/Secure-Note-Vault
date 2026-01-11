9956f0cef280ec78c3433d35d431fba7f8a021c7
Pratiksha Ankushrao <147415179+Pratiksha69394@users.noreply.github.com>
2026-01-11 11:12:48 +0530

    xyz

--- client/vite-project/src/api/api.js
+++ client/vite-project/src/api/api.js
@@ -1,7 +1,8 @@
 import axios from "axios";
+const API = import.meta.env.VITE_API_URL || "http://localhost:5000";
 
-const base = import.meta.env.VITE_API_URL
-  ? `${import.meta.env.VITE_API_URL}/api`
+const base = API
+  ? `${API}/api`
   : "http://localhost:5000/api";
 
 const api = axios.create({
