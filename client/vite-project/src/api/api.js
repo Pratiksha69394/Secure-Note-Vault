df3019e71b18af35038fd9d944a55faa4125a46f
Pratiksha Ankushrao <147415179+Pratiksha69394@users.noreply.github.com>
2026-01-11 11:09:07 +0530

    made changes

--- client/vite-project/src/api/api.js
+++ client/vite-project/src/api/api.js
@@ -1,10 +1,12 @@
 import axios from "axios";
-const API = import.meta.env.VITE_API_URL + "/api";
 
-const base = API || "http://localhost:5000/api";
+const base = import.meta.env.VITE_API_URL
+  ? `${import.meta.env.VITE_API_URL}/api`
+  : "http://localhost:5000/api";
 
 const api = axios.create({
-  baseURL: base
+  baseURL: base,
+  withCredentials: true
 });
 
 api.interceptors.request.use(config => {
